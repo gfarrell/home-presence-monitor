1e9f935ce5ffca59e8ff5e234e61a6f7430bd59e
Gideon Farrell <me@gideonfarrell.co.uk>
2014-09-21 00:55:10 +0100

    Added age fetching in arp.js

--- app/arp.js
+++ app/arp.js
@@ -1,11 +1,23 @@
 var fs = require('fs');
 var _  = require('lodash');
+var moment = require('moment');
+
+var _FILE = 'arp.log';
 
 module.exports = {
-    readFile: function(cb) {
-        var f = fs.readFile('arp.log', function(err, data) {
-            cb.call(this, this.parse(data.toString()));
+    read: function(cb) {
+        var f = fs.readFile(_FILE, function(err, data) {
+            if(err) throw new Error(err);
+            this.getTimestamp(function(ts) {
+                cb.call(this, {machines: this.parse(data.toString()), age: moment().diff(ts)});
+            }.bind(this));
         }.bind(this));
+    },
+
+    getTimestamp: function(cb) {
+        fs.stat(_FILE, function(err, stats) {
+            cb.call(this, moment(stats.mtime));
+        });
     },
 
     parse: function(data) {
